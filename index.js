5dcddc30184656454da28773c30c6102f35c254c
Luca Sbardella <luca@quantmind.com>
2017-09-15 08:27:53 +0100

    0.5.2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export {default as view} from './src/main';
 export {default as viewMount} from './src/mount';
+export {default as viewBase} from './src/core/base';
 export {default as viewModel} from './src/model/main';
 export {default as viewExpression} from './src/parser/expression';
 export {default as viewReady} from './src/utils/dom';
