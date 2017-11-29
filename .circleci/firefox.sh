9bfa0a188c4ea77b2ddf3846d363a225657e2bf3
Luca Sbardella <luca@quantmind.com>
2017-11-29 15:21:13 +0000

    browsers

--- .circleci/firefox.sh
+++ .circleci/firefox.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-apt-get update && sudo apt-get install libpango1.0-0 && sudo apt-get install firefox
-ln -sf /usr/lib/firefox/firefox /usr/bin/firefox
+sudo apt-get update && sudo apt-get install libpango1.0-0 && sudo apt-get install firefox
+sudo ln -sf /usr/lib/firefox/firefox /usr/bin/firefox
