b1146f4f2e259c383bbf10ab6b4e313a7420381c
Luca Sbardella <luca@quantmind.com>
2017-12-13 19:10:45 +0000

    no need for promise

--- test/promise.js
+++
@@ -1,8 +0,0 @@
-export default {
-    ok (result) {
-        return new Promise(function (resolve) {resolve(result);});
-    },
-    error (result) {
-        return new Promise(function (resolve, error) {error(result);});
-    }
-};
