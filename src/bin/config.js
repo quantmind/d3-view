afd4cd6a240d8559c481b0f5c219d69aefa7e588
Luca Sbardella <luca@quantmind.com>
2018-01-16 19:28:35 +0000

    fixed the mess

---
+++ src/bin/config.js
@@ -0,0 +1,18 @@
+import {realpathSync} from 'fs';
+
+import {handleError} from './error';
+
+
+export default function (configFile) {
+    configFile = realpathSync(configFile);
+
+	return Promise.resolve(require(configFile)).then(config => {
+		if (Object.keys(config).length === 0 ) {
+			handleError({
+				code: 'MISSING_CONFIG',
+				message: 'Config file must export an options object'
+			});
+		}
+		return config;
+	});
+}
