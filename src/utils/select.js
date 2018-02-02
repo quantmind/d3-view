c6360b22df9e58d0f371046cda04bc202302c556
Luca Sbardella <luca@quantmind.com>
2018-02-02 22:35:35 +0000

    few more arrows

--- src/utils/select.js
+++ src/utils/select.js
@@ -2,7 +2,4 @@ import {isFunction} from 'd3-let';
 import {select} from 'd3-selection';
 
 
-export default function (el) {
-    if (el && !isFunction(el.size)) return select(el);
-    return el;
-}
+export default el => (el && !isFunction(el.size)) ? select(el) : el;
